eb11a903523370e056491da0983dc3111aabff5f
bem-boi <beamp.2023@gmail.com>
2022-07-25 12:21:38 +0700

    Created recursion password generator

--- src/PasswordGenUtils.java
+++ src/PasswordGenUtils.java
@@ -1,15 +1,72 @@
-import java.lang.*;
+import java.util.Random;
+import java.util.regex.*;
 
 public final class PasswordGenUtils {
     // https://www.delftstack.com/howto/java/java-utility-class.java/
     // https://www.baeldung.com/java-generate-secure-password
     // https://kodejava.org/how-do-i-generate-a-random-alpha-numeric-string/
+    
+    // this checks for the password's conditions
+    // https://www.geeksforgeeks.org/how-to-validate-a-password-using-regular-expressions-in-java/ 
 
-    // private char[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v};
+    public static Random rand = new Random();
+
+    public static char[] chars = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
+    'n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5',
+    '6','7','8','9','!','@','$','%','^','&','#','-','+','='};
     
     private PasswordGenUtils(){
         throw new AssertionError("Instantiating utility class.");    
     }
 
+    // generates the password
+
+    public static String generatePassword(int n){
+        if(n == 1){
+            int num = rand.nextInt(46);
+            Boolean cap = rand.nextBoolean();
+            if(cap){
+                return Character.toUpperCase(chars[num]) + "";
+            }
+            return chars[num] + "";
+        }else{
+            int num = rand.nextInt(46);
+            return generatePassword(n-1) + chars[num];
+        }
+    }
+
+    // checks for the password's condition
+
+    public static boolean isValidPassword(String password){
+        String regex = "^(?=.*[0-9])"
+                        + "(?=.*[a-z])(?=.*[A-Z])"
+                        + "(?=.*[@#$%^&+=])"
+                        + "(?=\\S+$).{8,20}$";
+
+        Pattern p = Pattern.compile(regex);
+
+
+        if (password == null){
+            return false;
+        }
+
+        Matcher m = p.matcher(password);
+
+        return m.matches();
+    }
+
+    // just for check, can remove later
+
+    public static void main(String[] args) {
+        String password = "";
+
+        while (isValidPassword(password) == false) {
+            password = generatePassword(15);
+        }
+
+        System.out.println(password);
+        System.out.println(isValidPassword(password));
+
+    }
 
 }
